5109840364fb039d292be919d69504dd81b4a424
YOUR NAME <ameya.charnalia@gmail.com>
2022-09-06 17:00:15 -0400

    Updates

---
+++ precache-manifest.047be78ec0240422c80258f9a55a2902.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ecc456f7571515a17deb",
+    "url": "/portfolio/static/css/main.2b2cedf3.chunk.css"
+  },
+  {
+    "revision": "ecc456f7571515a17deb",
+    "url": "/portfolio/static/js/main.ecc456f7.chunk.js"
+  },
+  {
+    "revision": "6c373ff15b83456d26d1",
+    "url": "/portfolio/static/js/1.6c373ff1.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "ecca7223dcddad98a653dd934e0c016f",
+    "url": "/portfolio/index.html"
+  }
+];
