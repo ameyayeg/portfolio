6ecd9e2274e219b4fe6cafebb6754931babf3913
YOUR NAME <ameya.charnalia@gmail.com>
2022-09-09 13:02:02 -0400

    Updates

---
+++ precache-manifest.e2175d1b30aff79df82679b0a83e09b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "970417a3b09476d42b0b",
+    "url": "/portfolio/static/css/main.c2615006.chunk.css"
+  },
+  {
+    "revision": "970417a3b09476d42b0b",
+    "url": "/portfolio/static/js/main.970417a3.chunk.js"
+  },
+  {
+    "revision": "695b4889971f4a0a05d0",
+    "url": "/portfolio/static/js/1.695b4889.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "f247fb56f6a89bf93c6f9928d0c22c44",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.cc074c9af7421dbe392d90b7e148bec4.js"
+  "/portfolio/precache-manifest.e2175d1b30aff79df82679b0a83e09b8.js"
 );
 
 workbox.clientsClaim();
