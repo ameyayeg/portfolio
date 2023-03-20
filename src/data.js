7de1f01a62cc60994247dd73a1f0098efbf2f7f2
YOUR NAME <ameya.charnalia@gmail.com>
2023-03-20 12:41:11 -0400

    Changed the projects

--- src/data.js
+++ src/data.js
@@ -1,24 +1,22 @@
-
-
 const projects = [
-    {
-      title: "Quizzical",
-      tech: ["React", "Javascript"],
-      description:
-        "A quiz app made in React that makes API calls to the Open Trivia database.",
-      image: "images/quizzical.png",
-      live: "https://ameyayeg.github.io/quizzical/",
-      id: 1
-    },
-    {
-      title: "Billbot",
-      tech: ["Node", "Twitter API"],
-      description:
-        "A Twitter bot that tells you what government bills are up for debate daily.",
-      image: "images/billbot.png",
-      live: "https://twitter.com/billsbeepboop",
-      id: 2
-    },
-  ];
+  {
+    title: 'Eat the Strip',
+    tech: ['Next JS', 'Netlify CMS', 'React', 'Javascript'],
+    description:
+      'A food review blog that focuses on strip mall gems in the national capital region.',
+    image: 'images/eatthestrip.png',
+    live: 'https://eatthestrip.com/',
+    id: 1,
+  },
+  {
+    title: 'Billbot',
+    tech: ['Node', 'Mastodon API', 'AWS'],
+    description:
+      'A Mastodon bot that tells you what government bills are up for debate daily.',
+    image: 'images/mastodon.png',
+    live: 'https://mstdn.ca/@billbot',
+    id: 2,
+  },
+]
 
-  export default projects
+export default projects
