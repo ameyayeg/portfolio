811f99b33348ca9eff0e8a2dc5496c41e3370e22
YOUR NAME <ameya.charnalia@gmail.com>
2023-11-21 15:49:04 -0500

    Changed home page animation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,47 +1,44 @@
-
-import React from "react"
+import React from 'react'
 import styles from '../styles/Home.module.css'
-import Navbar from '../components/Nav/Navbar';
-import Footer from '../components/Footer';
-import TextTransition, { presets } from "react-text-transition";
-import { Link } from "react-router-dom"
+import Navbar from '../components/Nav/Navbar'
+import Footer from '../components/Footer'
+import TextTransition, { presets } from 'react-text-transition'
+import { Link } from 'react-router-dom'
 
-
-const TEXTS = [
-    "web developer",
-    "parliamentary staffer",
-    "journalist"
-  ];
+const TEXTS = ['web developer', 'food journalist']
 
 const Home = () => {
-const [index, setIndex] = React.useState(0);
+  const [index, setIndex] = React.useState(0)
 
   React.useEffect(() => {
-    
-    const intervalId = setInterval(() =>
-      setIndex(index => index + 1),
-      2500 // every 3 seconds
-    );
-    return () => clearTimeout(intervalId);
-  }, []);
+    const intervalId = setInterval(
+      () => setIndex((index) => index + 1),
+      6000 // every 3 seconds
+    )
+    return () => clearTimeout(intervalId)
+  }, [])
 
-  const arrayText = TEXTS[index % TEXTS.length]
-
-    return ( 
-        <div className="container">
-            <Navbar />
-            <img src="images/profile.jpg" alt="Author smiling" className={styles.homeImage} />
-            <h2 className={styles.homeHeader}>
-                Hi there, I'm an Ottawa-based&nbsp;
-                <TextTransition style={{color: '#1589FF'}} springConfig={presets.slow} inline={false}>
-                  {index < 3 ? 
-                  arrayText :
-                  <Link to="/work">web developer.</Link>}
-                </TextTransition>
-            </h2>
-            <Footer />
-        </div>
-     );
+  return (
+    <div className="container">
+      <Navbar />
+      <img
+        src="images/profile.jpg"
+        alt="Author smiling"
+        className={styles.homeImage}
+      />
+      <h2 className={styles.homeHeader}>
+        Hi there, I'm an Ottawa-based&nbsp;
+        <TextTransition
+          style={{ color: '#1589FF' }}
+          springConfig={presets.slow}
+          inline={false}
+        >
+          {TEXTS[index % TEXTS.length]}
+        </TextTransition>
+      </h2>
+      <Footer />
+    </div>
+  )
 }
- 
-export default Home;
+
+export default Home
